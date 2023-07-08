2e854a039b0a1487d9053519ecce4af31f49c666
JoeTheCookie217 <56717228+JoeTheCookie217@users.noreply.github.com>
2023-07-09 01:29:36 +0200

    add trpc provider

--- front/src/app.tsx
+++ front/src/app.tsx
@@ -1,15 +1,38 @@
 import React from "react";
 import "./app.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { httpBatchLink } from "@trpc/client";
 import Collection from "./components/Collection";
+import { api } from "./utils/config";
+import { trpc } from "./utils/trpc";
 
 const App: React.FC = () => {
-  return (
-    <main>
-      <div className="app">
-        <Collection />
-      </div>
-    </main>
-  );
+    const queryClient = new QueryClient({
+        defaultOptions: {
+            queries: {
+                refetchOnWindowFocus: false,
+            },
+        },
+    });
+    const trpcClient = trpc.createClient({
+        links: [
+            httpBatchLink({
+                url: api,
+            }),
+        ],
+    });
+
+    return (
+        <trpc.Provider client={trpcClient} queryClient={queryClient}>
+            <QueryClientProvider client={queryClient}>
+                <main>
+                    <div className="app">
+                        <Collection />
+                    </div>
+                </main>
+            </QueryClientProvider>
+        </trpc.Provider>
+    );
 };
 
 export default App;
